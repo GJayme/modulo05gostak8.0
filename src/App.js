d2e23e6d9ca12ddf87df354ec0ddd8b552b39582
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-03-18 12:25:33 -0300

    Rotas Main e Repository com React-Router-Dom.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
+import Routes from './routes';
+
 function App() {
-  return (
-    <div className="App">
-      <h2>Hello Word</h2>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
